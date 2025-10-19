73468f28f883148785490d3354c32a43215c4354
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2025-10-19 20:50:31 +0300

    feat: ecommerce service

---
+++ projects/yandex-metrika/src/lib/ym-ecommerce/ym-ecommerce-service.ts
@@ -0,0 +1,431 @@
+import { Injectable, inject, signal, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { libName } from '../ym-lib-name';
+import {
+  EcommerceActionField,
+  EcommerceActionType,
+  EcommerceData,
+  EcommerceProduct,
+  EcommercePromotion,
+} from './ym-ecommerce-interface';
+
+/**
+ * Сервис для работы с электронной коммерцией Яндекс.Метрики
+ *
+ * Предоставляет типобезопасный API для отправки данных о взаимодействиях
+ * пользователей с товарами в соответствии с официальной документацией Яндекс.Метрики.
+ *
+ * @remarks
+ * - Данные отправляются через глобальный массив dataLayer (или кастомное имя)
+ * - Полное соответствие формату Google Analytics Enhanced Ecommerce
+ * - Автоматическая проверка размера данных (ограничение 8192 символа)
+ * - SSR-совместимость
+ *
+ * @example
+ * // Отслеживание добавления товара в корзину
+ * this.ecommerce.executeAddToCart({
+ *   id: 'product-123',
+ *   name: 'Футболка',
+ *   price: 1000,
+ *   quantity: 1
+ * });
+ *
+ * @see https://yandex.ru/support/metrica/ru/ecommerce/data
+ */
+@Injectable({ providedIn: 'root' })
+export class YMEcommerceService {
+  readonly #isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+  readonly #defaultDataLayerName = signal<string | undefined>(undefined); //todo
+  readonly #dataLayersNames = signal<string[]>([]); //todo
+  readonly #maxDataLength = 8192;
+
+  /**
+   * Базовый метод для отправки данных электронной коммерции
+   *
+   * Формирует и отправляет данные в точном соответствии с форматом Яндекс.Метрики.
+   * Автоматически обрабатывает различные структуры данных для разных типов действий.
+   *
+   * @param actionType - Тип действия пользователя с товаром
+   * @param products - Список товаров (не используется для impressions, promoView, promoClick)
+   * @param actionField - Данные о действии (только для purchase)
+   * @param promotions - Данные о промо-кампаниях (только для promoView/promoClick)
+   * @param currencyCode - Код валюты (ISO 4217), по умолчанию 'RUB'
+   *
+   * @throws {Error} При превышении размера данных 8192 символа
+   *
+   * @example
+   * // Отправка данных о покупке
+   * this.pushEcommerceData(
+   *   'purchase',
+   *   products,
+   *   { id: 'order-123', revenue: 5000 },
+   *   [],
+   *   'RUB'
+   * );
+   */
+  public execute(
+    actionType: EcommerceActionType,
+    products: EcommerceProduct[] = [],
+    actionField?: EcommerceActionField,
+    promotions: EcommercePromotion[] = [],
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(
+      this.#defaultDataLayerName()!,
+      actionType,
+      products,
+      actionField,
+      promotions,
+      currencyCode,
+    );
+  }
+
+  /**
+   * Отправка данных электронной коммерции в указанный dataLayer
+   *
+   * @param dataLayer - Имя dataLayer для отправки данных
+   * @param actionType - Тип действия пользователя с товаром
+   * @param products - Список товаров (не используется для impressions, promoView, promoClick)
+   * @param actionField - Данные о действии (только для purchase)
+   * @param promotions - Данные о промо-кампаниях (только для promoView/promoClick)
+   * @param currencyCode - Код валюты (ISO 4217), по умолчанию 'RUB'
+   *
+   * @throws {Error} При превышении размера данных 8192 символа или недоступности dataLayer
+   */
+  public executeWithDataLayer(
+    dataLayer: string,
+    actionType: EcommerceActionType,
+    products: EcommerceProduct[] = [],
+    actionField?: EcommerceActionField,
+    promotions: EcommercePromotion[] = [],
+    currencyCode: string = 'RUB',
+  ): void {
+    if (!this.#isBrowser) return;
+
+    this.validateDataLayerAvailability(dataLayer);
+
+    try {
+      const pushData = this.buildEcommerceData(
+        actionType,
+        products,
+        actionField,
+        promotions,
+        currencyCode,
+      );
+
+      this.validateDataSize(pushData);
+      this.pushToDataLayer(dataLayer, pushData);
+    } catch (error) {
+      console.error(`${libName}: Ошибка отправки Ecommerce данных:`, error);
+      throw error;
+    }
+  }
+
+  /**
+   * Отслеживание просмотра списка товаров
+   *
+   * Используется когда пользователь видит список товаров (каталог, поисковая выдача).
+   * Данные должны отправляться в момент открытия списка товаров.
+   *
+   * @param products - Массив товаров в списке с позициями
+   * @param currencyCode - Код валюты (ISO 4217)
+   *
+   * @example
+   * this.executeProductImpressions([
+   *   { id: 'P15432', name: 'Футболка', price: 477.60, position: 1 },
+   *   { id: 'P15435', name: 'Футболка', price: 500.60, position: 2 }
+   * ]);
+   */
+  public executeProductImpressions(
+    products: EcommerceProduct[],
+    currencyCode: string = 'RUB',
+  ): void {
+    this.execute('impressions', products, undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание просмотра списка товаров в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param products - Массив товаров в списке с позициями
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeProductImpressionsWithDataLayer(
+    dataLayerName: string,
+    products: EcommerceProduct[],
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'impressions', products, undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание клика по товару из списка
+   *
+   * Вызывается когда пользователь кликает на товар для перехода к детальной странице.
+   * Данные должны передаваться в момент клика по ссылке товара.
+   *
+   * @param product - Данные товара по которому произведен клик
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeProductClick(product: EcommerceProduct, currencyCode: string = 'RUB'): void {
+    this.execute('click', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание клика по товару из списка в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param product - Данные товара по которому произведен клик
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeProductClickWithDataLayer(
+    dataLayerName: string,
+    product: EcommerceProduct,
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'click', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание просмотра карточки товара
+   *
+   * Вызывается при открытии страницы с детальной информацией о товаре.
+   * Данные должны отправляться в момент открытия страницы с карточкой товара.
+   *
+   * @param product - Данные просматриваемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeProductView(product: EcommerceProduct, currencyCode: string = 'RUB'): void {
+    this.execute('detail', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание просмотра карточки товара в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param product - Данные просматриваемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeProductViewWithDataLayer(
+    dataLayerName: string,
+    product: EcommerceProduct,
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'detail', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание добавления товара в корзину
+   *
+   * Вызывается при добавлении товара в корзину покупок.
+   * Данные должны отправляться в момент добавления заказа в корзину.
+   *
+   * @param product - Данные добавляемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeAddToCart(product: EcommerceProduct, currencyCode: string = 'RUB'): void {
+    this.execute('add', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание добавления товара в корзину в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param product - Данные добавляемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeAddToCartWithDataLayer(
+    dataLayerName: string,
+    product: EcommerceProduct,
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'add', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание удаления товара из корзины
+   *
+   * Вызывается при удалении товара из корзины покупок.
+   * Данные должны отправляться в момент удаления заказа из корзины.
+   *
+   * @param product - Данные удаляемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeRemoveFromCart(product: EcommerceProduct, currencyCode: string = 'RUB'): void {
+    this.execute('remove', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание удаления товара из корзины в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param product - Данные удаляемого товара
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executeRemoveFromCartWithDataLayer(
+    dataLayerName: string,
+    product: EcommerceProduct,
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'remove', [product], undefined, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание совершения покупки
+   *
+   * Вызывается при завершении заказа/покупки.
+   * Данные должны отправляться в момент подтверждения заказа.
+   *
+   * @param actionField - Данные о заказе (обязателен ID заказа)
+   * @param products - Список товаров в заказе
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executePurchase(
+    actionField: EcommerceActionField,
+    products: EcommerceProduct[],
+    currencyCode: string = 'RUB',
+  ): void {
+    this.execute('purchase', products, actionField, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание совершения покупки в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param actionField - Данные о заказе (обязателен ID заказа)
+   * @param products - Список товаров в заказе
+   * @param currencyCode - Код валюты (ISO 4217)
+   */
+  public executePurchaseWithDataLayer(
+    dataLayerName: string,
+    actionField: EcommerceActionField,
+    products: EcommerceProduct[],
+    currencyCode: string = 'RUB',
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'purchase', products, actionField, [], currencyCode);
+  }
+
+  /**
+   * Отслеживание просмотра внутренней рекламы
+   *
+   * Вызывается когда пользователь увидел промо-материалы на сайте.
+   * Данные должны отправляться при просмотре рекламных материалов.
+   *
+   * @param promotions - Данные о просмотренных промо-кампаниях
+   */
+  public executePromoView(promotions: EcommercePromotion[]): void {
+    this.execute('promoView', [], undefined, promotions);
+  }
+
+  /**
+   * Отслеживание просмотра внутренней рекламы в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param promotions - Данные о просмотренных промо-кампаниях
+   */
+  public executePromoViewWithDataLayer(
+    dataLayerName: string,
+    promotions: EcommercePromotion[],
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'promoView', [], undefined, promotions);
+  }
+
+  /**
+   * Отслеживание клика по внутренней рекламе
+   *
+   * Вызывается при клике на промо-материалы на сайте.
+   * Данные должны отправляться при клике по рекламному материалу.
+   *
+   * @param promotion - Данные о промо-кампании по которой произведен клик
+   */
+  public executePromoClick(promotion: EcommercePromotion): void {
+    this.execute('promoClick', [], undefined, [promotion]);
+  }
+
+  /**
+   * Отслеживание клика по внутренней рекламе в указанный dataLayer
+   *
+   * @param dataLayerName - Имя dataLayer для отправки данных
+   * @param promotion - Данные о промо-кампании по которой произведен клик
+   */
+  public executePromoClickWithDataLayer(
+    dataLayerName: string,
+    promotion: EcommercePromotion,
+  ): void {
+    this.executeWithDataLayer(dataLayerName, 'promoClick', [], undefined, [promotion]);
+  }
+
+  /**
+   * Проверяет размер данных согласно ограничению Яндекс.Метрики (8192 символа)
+   *
+   * @param data - Данные для проверки
+   * @throws {Error} Если размер данных превышает 8192 символа
+   */
+  private validateDataSize(data: any): void {
+    const dataString = JSON.stringify(data);
+
+    if (dataString.length > this.#maxDataLength) {
+      const errorMessage = `${libName}: Размер Ecommerce данных (${dataString.length} символов) превышает максимально допустимый размер 8192 символа. Рекомендуется разбить данные на части.`;
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+  }
+
+  /**
+   * Создает объект данных для электронной коммерции
+   */
+  private buildEcommerceData(
+    actionType: EcommerceActionType,
+    products: EcommerceProduct[],
+    actionField?: EcommerceActionField,
+    promotions: EcommercePromotion[] = [],
+    currencyCode: string = 'RUB',
+  ): { ecommerce: EcommerceData } {
+    const ecommerceData: EcommerceData = { currencyCode };
+
+    switch (actionType) {
+      case 'impressions':
+        ecommerceData['impressions'] = products;
+        break;
+
+      case 'promoView':
+      case 'promoClick':
+        ecommerceData[actionType] = { promotions };
+        break;
+
+      default:
+        const actionData: any = {};
+
+        if (actionType === 'purchase' && actionField) {
+          actionData.actionField = actionField;
+        }
+
+        if (products.length > 0) {
+          actionData.products = products;
+        }
+
+        ecommerceData[actionType] = actionData;
+        break;
+    }
+
+    return { ecommerce: ecommerceData };
+  }
+
+  /**
+   * Отправляет данные в указанный dataLayer
+   */
+  private pushToDataLayer(dataLayer: string, data: any): void {
+    (window as any)[dataLayer].push(data);
+  }
+
+  /**
+   * Проверяет доступность dataLayer
+   */
+  private validateDataLayerAvailability(dataLayer: string): void {
+    if (!(window as any)[dataLayer])
+      throw new Error(`DataLayer "${dataLayer}" не найден. Убедитесь, что он инициализирован.`);
+
+    if (!Array.isArray((window as any)[dataLayer]))
+      throw new Error(`DataLayer "${dataLayer}" должен быть массивом`);
+  }
+}
--- projects/yandex-metrika/src/lib/ym-init-service.ts
+++ projects/yandex-metrika/src/lib/ym-init-service.ts
@@ -1,4 +1,4 @@
-import { DOCUMENT, inject, Injectable, isDevMode, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, inject, Injectable, isDevMode, PLATFORM_ID, signal } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { YMConfig } from './ym-config-interface';
 import { libName } from './ym-lib-name';
@@ -7,35 +7,84 @@ import { libName } from './ym-lib-name';
 export class YMInitService {
   readonly #document = inject(DOCUMENT);
   readonly #isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
   readonly #defaultScriptUrl = 'https://mc.yandex.ru/metrika/tag.js';
+  readonly #initialized = signal(false);
+  readonly #loadedScripts = signal<number[]>([]);
+
+  readonly #defaultDataLayerName = 'dataLayer';
+  readonly #dataLayersNames = signal<string[]>([]);
 
   public initialize(config: YMConfig): void {
     if (!this.canInit(config)) return;
-
-    this.loadScript(config);
+    if (config.options?.ecommerce) this.initializeEcommerceDataLayer(config.options.ecommerce);
+    if (!this.#initialized()) {
+      this.loadScript(config);
+      this.#initialized.set(true);
+    }
+    this.initializeCounter(config);
     this.addNoscriptFallback(config.id);
   }
 
-  private canInit(config: YMConfig): boolean {
-    return this.#isBrowser && !!config && !!config.id && (!config?.prodOnly || !isDevMode());
+  private initializeCounter(config: YMConfig): void {
+    this.initializeYMQueue();
+
+    (window as any).ym(config.id, 'init', config.options);
+
+    this.#loadedScripts.update((scripts) => [...scripts, config.id]);
+
+    console.log(`${libName}: Счетчик ${config.id} инициализирован`);
   }
 
   private loadScript(config: YMConfig): void {
-    this.initializeYMQueue();
-
     const script = this.#document.createElement('script');
     script.src = config?.alternativeScriptUrl || this.#defaultScriptUrl;
     script.async = config.loading === 'async';
     script.defer = config.loading === 'defer';
 
-    (window as any).ym(config.id, 'init', config.options);
+    script.onload = () => {
+      console.log(`${libName}: Скрипт Яндекс.Метрики успешно загружен`);
+      console.log(`${libName}: Инициализировано счетчиков: ${this.#loadedScripts().length}`);
+    };
 
-    script.onload = () =>
-      console.log(`${libName}: Скрипт для счетчика ${config.id} успешно загружен`);
     script.onerror = (error) =>
-      console.error(`${libName}: Не удалось загрузить скрипт для счетчика ${config.id}: `, error);
+      console.error(`${libName}: Не удалось загрузить скрипт Яндекс.Метрики: `, error);
 
     this.#document.head.appendChild(script);
+  }
+
+  private initializeEcommerceDataLayer(ecommerce: boolean | string | any[]): void {
+    let dataLayerName: string;
+
+    if (ecommerce === true) {
+      dataLayerName = this.#defaultDataLayerName;
+    } else if (typeof ecommerce === 'string' && ecommerce.trim().length > 0) {
+      dataLayerName = ecommerce.trim();
+    } else if (Array.isArray(ecommerce)) {
+      console.warn(`${libName}: Массив для ecommerce пока не поддерживается`);
+      return;
+    } else {
+      console.warn(`${libName}: Неверный формат ecommerce:`, ecommerce);
+      return;
+    }
+
+    if (this.#dataLayersNames().includes(dataLayerName)) {
+      console.warn(`${libName}: DataLayer "${dataLayerName}" для ecommerce уже инициализирован`);
+      return;
+    }
+
+    if (!Array.isArray((window as any)[dataLayerName])) {
+      (window as any)[dataLayerName] = [];
+      console.log(`${libName}: DataLayer "${dataLayerName}" создан для ecommerce`);
+    } else {
+      console.log(`${libName}: DataLayer "${dataLayerName}" уже существует`);
+    }
+
+    this.#dataLayersNames.update((layers) => [...layers, dataLayerName]);
+  }
+
+  private canInit(config: YMConfig): boolean {
+    return this.#isBrowser && !!config && !!config.id && (!config?.prodOnly || !isDevMode());
   }
 
   private addNoscriptFallback(id: number): void {
